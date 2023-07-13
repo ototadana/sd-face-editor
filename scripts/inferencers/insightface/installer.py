81923e35559760d310d61b808e5cf04c0b4c757d
ototadana <ototadana@gmail.com>
2023-07-13 22:45:48 +0900

    fix install error

--- scripts/inferencers/insightface/installer.py
+++ scripts/inferencers/insightface/installer.py
@@ -8,4 +8,13 @@ class InsightFaceInstaller(Installer):
         return "InsightFace"
 
     def requirements(self) -> List[str]:
-        return ["insightface"]
+        return ['"insightface>=0.7.3"', "onnxruntime"]
+
+    def install(self) -> None:
+        try:
+            from scripts.inferencers.insightface.detector import InsightFaceDetector
+
+            InsightFaceDetector()
+        except Exception:
+            super().install()
+        return None
