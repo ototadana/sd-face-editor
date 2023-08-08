84d3d2e80dfdebbaeca9b23c06233bca0b29d559
ototadana <ototadana@gmail.com>
2023-08-09 00:16:26 +0900

    add mmseg mask generator

---
+++ scripts/inferencers/openmmlab/installer.py
@@ -0,0 +1,16 @@
+import launch
+
+from scripts.use_cases.installer import Installer
+
+
+class OpenMMLabInstaller(Installer):
+    def name(self) -> str:
+        return "OpenMMLab"
+
+    def install(self) -> None:
+        launch.run_pip(
+            'install openmim "mmsegmentation>=1.0.0" huggingface_hub',
+            "requirements for openmmlab inferencers of Face Editor",
+        )
+        launch.run("mim install mmengine")
+        launch.run('mim install "mmcv>=2.0.0"')
